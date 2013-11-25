566f5d9c01160ca998af9cb2749a7643992d0b57
Nick Ewing <nick@nickewing.net>
2013-11-24 16:17:57 -0800

    Update interface

--- remote.rb
+++ remote.rb
@@ -2,16 +2,10 @@ require "hacky_hal"
 require "yaml"
 
 class Remote
-  attr_reader :devices
-
   DEVICE_FILE = "./devices.yml"
 
   def initialize
-    HackyHAL::Registry.instance.load_yaml_file(DEVICE_FILE)
-  end
-
-  def devices
-    HackyHAL::Registry.instance.devices
+    define_device_methods
   end
 
   def set_input_output(options)
@@ -19,18 +13,174 @@ class Remote
     avr_output = options["av_receiver_output"].split(",")
     switch_input = options["secondary_monitor_switch_input"].to_i
 
-    avr_projector_output = avr_output.include?(devices[:projector][:av_receiver_output])
-    avr_monitor_output = avr_output.include?(devices[:primary_monitor][:av_receiver_output])
+    avr_projector_output = avr_output.include?(projector[:av_receiver_output])
+    avr_monitor_output = avr_output.include?(primary_monitor[:av_receiver_output])
 
     setup_av_reciever(avr_input)
-    devices[:av_receiver].set_hdmi_output("OUT_1", avr_projector_output)
-    devices[:av_receiver].set_hdmi_output("OUT_2", avr_monitor_output)
+    av_receiver.set_hdmi_output("OUT_1", avr_projector_output)
+    av_receiver.set_hdmi_output("OUT_2", avr_monitor_output)
 
-    devices[:secondary_monitor_switch].input = switch_input
+    secondary_monitor_switch.input = switch_input
 
+    set_laptop_audio_output(avr_input, avr_projector_output)
+    set_laptop_mirroring(avr_input, switch_input)
+
+    set_pc_mirroring(avr_input, switch_input)
+    reset_pc_screens
+  end
+
+  def toggle_av_receiver_power
+    HackyHAL::Log.instance.debug(av_receiver.on)
+    av_receiver.on = !av_receiver.on
+  end
+
+  def toggle_projector_power
+    projector.on = !projector.on
+  end
+
+  def increase_volume(options)
+    av_receiver.volume += options["amount"].to_i || 1
+  end
+
+  def mute
+    av_receiver.mute = !av_receiver.mute
+  end
+
+  def rain
+    laptop.exec("open 'http://rain.simplynoise.com'")
+    set_laptop_audio_output_device("RX-A1020 90E681")
+    disable_avr_output
+    secondary_monitor_switch.input = secondary_monitor_switch[:unused_input]
+    av_receiver.volume = -45.0
+    projector.on = false
+  end
+
+  def all_off
+    disable_avr_output
+    av_receiver.on = false
+    projector.on = false
+  end
+
+  def laptop_on_both_monitors
+    set_input_output(
+      "av_receiver_input" => laptop[:av_receiver_input],
+      "av_receiver_output" => primary_monitor[:av_receiver_output],
+      "secondary_monitor_switch_input" => laptop[:secondary_monitor_switch_input]
+    )
+  end
+
+  def pc_on_both_monitors
+    set_input_output(
+      "av_receiver_input" => pc[:av_receiver_input],
+      "av_receiver_output" => primary_monitor[:av_receiver_output],
+      "secondary_monitor_switch_input" => pc[:secondary_monitor_switch_input]
+    )
+  end
+
+  def ps3_on_projector
+    projector.on = true
+    set_input_output(
+      "av_receiver_input" => ps3[:av_receiver_input],
+      "av_receiver_output" => projector[:av_receiver_output],
+      "secondary_monitor_switch_input" => secondary_monitor_switch[:unused_input]
+    )
+  end
+
+  def netflix
+    open_roku_app("Netflix")
+  end
+
+  def amazon_video
+    open_roku_app("Amazon Instant Video")
+  end
+
+  def set_laptop_audio_output_device(name)
+    laptop.set_audio_output_device(name)
+  end
+
+  # def disconnect
+    # projector.disconnect
+    # laptop.disconnect
+    # pc.disconnect
+  # end
+
+  # def projector_lamp_hours
+    # projector.lamp_hours
+  # end
+
+  private
+
+  def devices
+    HackyHAL::Registry.instance.devices
+  end
+
+  def define_device_methods
+    registry = HackyHAL::Registry.instance
+    registry.load_yaml_file(DEVICE_FILE)
+
+    registry.devices.each_pair do |device_name, device|
+      define_singleton_method device_name do
+        device
+      end
+    end
+  end
+
+  def setup_av_reciever(input = AV_RECEIVER_DEFAULT_INPUT)
+    av_receiver.input = input
+    av_receiver.on = true
+    av_receiver.mute = false
+    # av_receiver.volume = -30.0
+  end
+
+  def disable_avr_output
+    av_receiver.set_hdmi_output("OUT_1", false)
+    av_receiver.set_hdmi_output("OUT_2", false)
+  end
+
+  def mirror_pc_screens
+    pc.mirror_screens("HDMI-0", "DVI-D-0")
+  end
+
+  def unmirror_pc_screens
+    pc.set_screen_position("DVI-D-0", "HDMI-0", :left)
+  end
+
+  # This fixes causes the PC to output with the correct display settings when
+  # switching from the monitor to the projector
+  def reset_pc_screens
+    pc.reset_display_settings("HDMI-0")
+  end
+
+  def mirror_laptop_screens
+    laptop.mirror_screens
+  end
+
+  def unmirror_laptop_screens
+    laptop.unmirror_screens
+  end
+
+  def set_laptop_mirroring(avr_input, switch_input)
+    if avr_input == laptop[:av_receiver_input] &&
+        switch_input == laptop[:secondary_monitor_switch_input]
+      unmirror_laptop_screens
+    else
+      mirror_laptop_screens
+    end
+  end
+
+  def set_pc_mirroring(avr_input, switch_input)
+    if avr_input == pc[:av_receiver_input] &&
+        switch_input == pc[:secondary_monitor_switch_input]
+      unmirror_pc_screens
+    else
+      mirror_pc_screens
+    end
+  end
+
+  def set_laptop_audio_output(avr_input, avr_projector_output)
     # TODO: Possibly clean this up by getting the list of available audio
     # outputs
-    if avr_input == devices[:laptop][:av_receiver_input]
+    if avr_input == laptop[:av_receiver_input]
       if avr_projector_output
         set_laptop_audio_output_device("EPSON PJ")
       else
@@ -39,94 +189,5 @@ class Remote
     else
       set_laptop_audio_output_device("Internal Speakers")
     end
-
-    if avr_input == devices[:laptop][:av_receiver_input] &&
-        switch_input == devices[:laptop][:secondary_monitor_switch_input]
-      unmirror_laptop_screens
-    else
-      mirror_laptop_screens
-    end
-
-    if avr_input == devices[:pc][:av_receiver_input] &&
-        switch_input == devices[:pc][:secondary_monitor_switch_input]
-      unmirror_pc_screens
-    else
-      mirror_pc_screens
-    end
-
-    reset_pc_screens
-  end
-
-  def toggle_av_receiver_power
-    HackyHAL::Log.debug(devices[:av_receiver].on)
-    devices[:av_receiver].on = !devices[:av_receiver].on
-  end
-
-  def toggle_projector_power
-    devices[:projector].on = !devices[:projector].on
-  end
-
-  def increase_volume(options)
-    devices[:av_receiver].volume += options["amount"].to_i || 1
-  end
-
-  def mute
-    devices[:av_receiver].mute = !devices[:av_receiver].mute
-  end
-
-  def rain
-    devices[:laptop].exec("open 'http://rain.simplynoise.com'")
-    set_laptop_audio_output_device("RX-A1020 90E681")
-
-    devices[:av_receiver].set_hdmi_output("OUT_1", false)
-    devices[:av_receiver].set_hdmi_output("OUT_2", false)
-
-    devices[:secondary_monitor_switch].input =
-      devices[:secondary_monitor_switch][:unused_input]
-
-    devices[:projector].on = false
-  end
-
-  def mirror_pc_screens
-    devices[:pc].mirror_screens("HDMI-0", "DVI-D-0")
-  end
-
-  def unmirror_pc_screens
-    devices[:pc].set_screen_position("DVI-D-0", "HDMI-0", :left)
-  end
-
-  def reset_pc_screens
-    devices[:pc].reset_display_settings("HDMI-0")
-  end
-
-  def mirror_laptop_screens
-    devices[:laptop].mirror_screens
-  end
-
-  def unmirror_laptop_screens
-    devices[:laptop].unmirror_screens
-  end
-
-  def set_laptop_audio_output_device(name)
-    devices[:laptop].set_audio_output_device(name)
-  end
-
-  def disconnect
-    devices[:projector].disconnect
-    devices[:laptop].disconnect
-    devices[:pc].disconnect
-  end
-
-  def projector_lamp_hours
-    devices[:projector].lamp_hours
-  end
-
-  private
-
-  def setup_av_reciever(input = AV_RECEIVER_DEFAULT_INPUT)
-    devices[:av_receiver].input = input
-    devices[:av_receiver].on = true
-    devices[:av_receiver].mute = false
-    # devices[:av_receiver].volume = -30.0
   end
 end
